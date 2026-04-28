d1b2da8fb5f3325c59e8f7882f489d9cdd91cf43
N2O <hank_elcaballero@hotmail.com>
2026-04-28 12:23:00 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,125 +14,35 @@ fi
 
 ruta=$(pwd)
 
-# Actualizando el sistema
+# Primera parte
+echo -e "Ejecutando primera parte"
+cd
+git clone https://github.com/xJackSx/BSPWMparrot.git
+cd BSPWMparrot
+chmod +x install.sh
+./install.sh
 
-sudo apt update
+echo -e "Primera parte ejecutada"
 
-sudo parrot-upgrade
+# Segunda parte 
+echo -e "Segunda parte en ejecucion"
 
-# Instalando dependencias de Entorno
+cd
+~/github
 
-sudo apt install -y build-essential git vim xcb libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-xinerama0-dev libasound2-dev libxcb-xtest0-dev libxcb-shape0-dev
-
-# Instalando Requerimientos para la polybar
-
-sudo apt install -y cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev libjsoncpp-dev libmpdclient-dev libuv1-dev libnl-genl-3-dev
-
-# Dependencias de Picom
-
-sudo apt install -y meson libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev libxcb-glx0-dev libpcre3 libpcre3-dev
-
-# Instalamos paquetes adionales
-
-sudo apt install -y feh flameshot scrub zsh rofi xclip bat locate wmname acpi bspwm sxhkd imagemagick ranger
-
-# Creando carpeta de Reposistorios
-
-mkdir ~/github
-
-# Descargar Repositorios Necesarios
-
-cd ~/github
-git clone --recursive https://github.com/polybar/polybar
-git clone https://github.com/ibhagwan/picom.git
-
-# Instalando Polybar
-
-cd ~/github/polybar
-mkdir build
-cd build
-cmake ..
-make -j$(nproc)
-sudo make install
-
-# Instalando Picom
-
-cd ~/github/picom
-git submodule update --init --recursive
-meson --buildtype=release . build
-ninja -C build
-sudo ninja -C build install
-
-# Instalando p10k
-
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.powerlevel10k
-echo 'source ~/.powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
-
-# Instalando p10k root
-
-sudo git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /root/.powerlevel10k
-
-# Temas de Rofi:
-
-mkdir -p ~/.config/rofi/themes
-cp $ruta/rofi/* ~/.config/rofi/themes/
-cp $ruta/rofi/template ~/.config/rofi/themes/.
-
-# Instalamos las HackNerdFonts
-
-sudo cp -v $ruta/fonts/HNF/* /usr/local/share/fonts/
-
-# Instalando Fuentes de Polybar
-
-sudo cp -v $ruta/Config/polybar/fonts/* /usr/share/fonts/truetype/
-
-# Instalando Wallpaper de S4vitar
-
-mkdir ~/Wallpaper
+# Instalando Wallpaper propios
+rm -rf ~/Wallpaper/*
 cp -v $ruta/Wallpaper/* ~/Wallpaper
 
-# Copiando Archivos de Configuración
-
-cp -rv $ruta/Config/* ~/.config/
-sudo cp -rv $ruta/kitty /opt/
-
-# Kitty Root
-
-sudo cp -rv $ruta/Config/kitty /root/.config/
-
-# Copia de configuracion de .p10k.zsh y .zshrc
+# Copia de configuracion de .nanorc y .zshrc
 
 rm -rf ~/.zshrc
 cp -v $ruta/.zshrc ~/.zshrc
 cp -v $ruta/.nanorc ~/.nanorc
 
-cp -v $ruta/.p10k.zsh ~/.p10k.zsh
-sudo cp -v $ruta/.p10k.zsh-root /root/.p10k.zsh
-
 # Script
-
-sudo cp -v $ruta/scripts/whichSystem.py /usr/local/bin/
 sudo cp -v $ruta/scripts/autonmap /.local/bin/
 
-
-# Plugins ZSH
-
-sudo apt install -y zsh-syntax-highlighting zsh-autosuggestions zsh-autocomplete
-sudo mkdir /usr/share/zsh-sudo
-cd /usr/share/zsh-sudo
-sudo wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/sudo/sudo.plugin.zsh
-
-# Cambiando de SHELL a zsh
-
-chsh -s /usr/bin/zsh
-sudo usermod --shell /usr/bin/zsh root
-sudo ln -s -fv ~/.zshrc /root/.zshrc
-
-# Configuramos el Tema de Rofi
-
-rofi-theme-selector
-
-echo -e "Primera parte terminada"
 sleep 5
 
 # instalar snap y flatpak
@@ -182,14 +92,6 @@ sudo flatpak repair
 
 sudo apt install arandr
 sudo apt install blueman bluez bluez-tools pulseaudio-module-bluetooth -y
-
-# Instalando Wallpaper de S4vitar
-
-mkdir ~/Wallpaper
-cp -av $ruta/Wallpaper/* ~/Wallpaper
-
-#Instalando xclip
-sudo apt install xclip
 
 #Crontab para la actualizacion automatica de updates
 sudo crontab -l > /tmp/micron 2>/dev/null
